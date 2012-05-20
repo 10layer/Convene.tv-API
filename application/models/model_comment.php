442d47e35be2ecacc1621ed32942260462af5c50
j-norwood-young <jason@10layer.com>
2012-05-20 09:20:35 +0200

    Show only live comments, and some admin features

--- application/models/model_comment.php
+++ application/models/model_comment.php
@@ -1,13 +1,17 @@
 <?php
 	class Model_Comment extends CI_Model {
 		
+		public function get_comment($comment_id) {
+			return $this->db->get_where("comments", array("id"=>$comment_id))->row();
+		}
+		
 		public function get_by_article($urlid, $publication_id) {
 			$this->db->select("comments.*");
 			$this->db->select("0 AS level", false);
 			$this->db->select("users.fname, users.sname");
 			$this->db->from("comments");
 			$this->db->join("users","users.id=comments.user_id");
-			$this->db->where("urlid",$urlid)->where("publication_id", $publication_id);
+			$this->db->where("urlid",$urlid)->where("publication_id", $publication_id)->where("live",true);
 			//$this->db->where("parent_id",0);
 			$this->db->order_by("comments.date_created");
 			$query=$this->db->get();
@@ -15,7 +19,7 @@
 		}
 		
 		public function count_by_article($urlid, $publication_id) {
-			return $this->db->select("*")->from("comments")->where("urlid",$urlid)->where("publication_id",$publication_id)->get()->num_rows();
+			return $this->db->select("*")->from("comments")->where("urlid",$urlid)->where("publication_id",$publication_id)->where("live",true)->get()->num_rows();
 		}
 
 		public function get_children($id) {
@@ -36,13 +40,12 @@
 			return false;
 		}
 
-		public function getall_admin($start=0,$limit=5) {
-			$this->db->select("comments.id,comments.comment, comments.date_created");
+		public function getall_admin($start=0,$limit=25, $publication_id) {
+			$this->db->select("comments.id,comments.comment, comments.date_created, comments.urlid, comments.live");
 			$this->db->select("users.fname, users.sname, users.id AS user_id");
-			$this->db->select("articles.headline, articles.urlid");
 			$this->db->from("comments");
 			$this->db->join("users","users.id=comments.user_id");
-			$this->db->join("articles","articles.id=comments.article_id");
+			$this->db->where("publication_id", $publication_id);
 			$this->db->order_by("date_created DESC");
 			$this->db->limit($limit,$start);
 			$query=$this->db->get();
@@ -90,6 +93,20 @@
 				"publication_id"=>$publication_id
 			);
 			$this->db->insert("comments",$dbdata);
+			return true;
+		}
+		
+		public function count_all($publication_id) {
+			return $this->db->where("publication_id", $publication_id)->get("comments")->num_rows();
+		}
+		
+		public function unlive($comment_id) {
+			$this->db->where("id",$comment_id)->update("comments", array("live"=>false));
+			return true;
+		}
+		
+		public function live($comment_id) {
+			$this->db->where("id",$comment_id)->update("comments", array("live"=>true));
 			return true;
 		}
 	}
