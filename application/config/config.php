963d22d2e8ef72a39fb484327631bfad10f3b862
j-norwood-young <jason@10layer.com>
2012-05-20 10:27:42 +0200

    Don't do domain-specific cookies

--- application/config/config.php
+++ application/config/config.php
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "convene_";
-$config['cookie_domain']	= ".convene.tv";
+$config['cookie_domain']	= "";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
